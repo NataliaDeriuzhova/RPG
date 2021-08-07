9cdf5c0f69b62a70081f9a9c77d171f87f85a4b4
Natalia Deriuzhova <natalia.deriuzhova@nutson.us>
2021-08-07 12:57:15 +0300

    fix master branch

--- enums/action_enums.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class ActionEnum(Enum):
-    FIGHT = 'fight'
-    ITEM = 'item'
-    TOTEM = 'totem'
--- enums/result_enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ResultEnum(Enum):
-    LOOSER = "ПОРАЖЕНИЕ"
-    WINNER = "ПОБЕДА"
